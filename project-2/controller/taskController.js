5ce6599260f274106043c261980048b0fa2afb44
arunaryan20 <arunkumar2031999@gmail.com>
2025-01-18 19:43:41 +0530

    project created successfuly

--- project-2/controller/taskController.js
+++ project-2/controller/taskController.js
@@ -1,13 +1,52 @@
 const taskModel=require("../models/taskModel");
 exports.createTask=async(req,res)=>{
         try{
-                const data=req.body;
-                let temp={
-                    name:data.name
+                const {name}=req.body;
+                let data={
+                    name:name
                 }
-                const result= await taskModel.create(temp);
-                res.status(200).json({code:200,message:"Data Inserted"})
+                await taskModel.create(data);
+                res.status(200).json({code:201,message:"Data Inserted"})
         }catch(err){
-            res.status(500).json({success:false,message:"Internal server error",Error:err});
+            res.status(500).json({code:500,message:"Internal server error",Error:err});
         }
+}
+
+exports.getNameList=async(req,res)=>{
+              try{
+                     const result=await taskModel.find();       
+                      res.status(200).json({code:200,message:"Task list",Data:result})
+              }catch(err){
+                res.status(500).json({code:500,message:"Internal server error",Error:err})
+              }
+}
+
+exports.deleteTask=async(req,res)=>{
+    try{
+             const id=req.params.id;
+             const filter={
+                _id:id
+             }
+        await taskModel.findOneAndDelete(filter);
+        res.status(200).json({code:200,message:"Data deleted successfuly"});
+    }catch(err){
+        res.status(500).json({code:500,message:"Internal server error",Error:err});
+    }
+}
+
+exports.updateTask=async(req,res)=>{
+         try{
+                 const id=req.params.id;
+                 const {name}=req.body;
+                 const filter={
+                    _id:id
+                 }   
+                 const data={
+                    name:name
+                 } 
+                 await taskModel.findByIdAndUpdate(filter,data);
+              res.status(200).json({code:200,message:"Task updated successfuly"});
+         }catch(err){
+            res.status(500).json({code:500,message:"Internal server error",Error:err});
+         }
 }
--- project-2/routes/taskRoutes.js
+++ project-2/routes/taskRoutes.js
@@ -5,6 +5,8 @@ const taskRoute=express.Router();
 const taskController=require("../controller/taskController");
 
 taskRoute.post("/create-task",taskController.createTask);
-
+taskRoute.get("/task-list",taskController.getNameList);
+taskRoute.delete("/delete-task/:id",taskController.deleteTask);
+taskRoute.put("/update-task/:id",taskController.updateTask);
 
 module.exports=taskRoute;
