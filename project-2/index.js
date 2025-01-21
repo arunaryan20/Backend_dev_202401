88586ba09a555e43b9d570b1ac962f5472455ef9
arunaryan20 <arunkumar2031999@gmail.com>
2025-01-21 19:48:10 +0530

    code updated

--- project-2/index.js
+++ project-2/index.js
@@ -2,15 +2,24 @@
 
 const express=require("express");
 const server=express();
+const cors=require("cors");
 require("dotenv").config();
 // Database connection
 const databaseConnection=require("./config/db");
 databaseConnection.dbConnect();
 
 server.use(express.json());
+var corsOptions = {
+     origin: 'http://localhost:3000', // Ensure this matches the frontend origin exactly
+     methods: 'GET,POST,PUT,DELETE', // Allowed methods
+     credentials: true, // Include cookies if needed
+ };
 
+server.use(cors(corsOptions));
 // importing router
 const taskRouter=require("./routes/taskRoutes");
+
+
 
 server.use("/api",taskRouter);
 
